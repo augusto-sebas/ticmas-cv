fb46b479321e84f1613073f4f84e98333f492f8c
augusto <seba_a_contreras@live.com.ar>
2023-02-28 19:32:33 -0300

    primer commit

---
+++ docs/index.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("mim");
+
+btn.addEventListener("click", () => {
+	console.log('Hola');
+	const fot = document.querySelector("footer");
+	fot.style.display = "block";
+	});
